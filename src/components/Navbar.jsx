98a8b47b1dba4e3017efce9b7dc0d1d4ece1df54
uday kumar <udaykumar@inr.group>
2022-08-20 14:33:50 +0530

    project init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react'
+import { close, logo, menu } from "../assets"
+
+const Navbar = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from "./Navbar";
+import Biling from "./Biling";
+import Buisness from "./Buisness"
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+    Navbar,
+    Biling,
+    Buisness,
+    Button,
+    CardDeal, Clients, CTA, FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero, Stats,
+    Testimonials
+};
